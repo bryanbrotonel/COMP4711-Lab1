685a25dacf4c27720f853f3cbea1666d49b0eba4
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-01-10 20:21:35 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # COMP4711-Lab1
-Lab #1 - Development Environment
+COMP4711 Lab #1 - Development Environment
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>COMP4711 Lab #1 - Development Environment</title>
+  </head>
+  <body>
+    <?php
+    echo "Hello world";
+     ?>
+  </body>
+</html>
